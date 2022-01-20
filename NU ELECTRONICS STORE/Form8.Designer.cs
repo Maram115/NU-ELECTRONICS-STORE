f5c21133465155eacd807a6ade700c985f7b8bb5
Maram Reda Abdelhakem zOGHIEB <Ma.Reda@nu.edu.eg>
2022-01-20 22:20:13 +0200

    form 8 change

--- NU ELECTRONICS STORE/Form8.Designer.cs
+++ NU ELECTRONICS STORE/Form8.Designer.cs
@@ -68,7 +68,7 @@
             this.pan.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.pan.Controls.Add(this.label2);
             this.pan.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.pan.Location = new System.Drawing.Point(-69, -9);
+            this.pan.Location = new System.Drawing.Point(-69, -14);
             this.pan.Name = "pan";
             this.pan.Size = new System.Drawing.Size(903, 59);
             this.pan.TabIndex = 6;
@@ -77,7 +77,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Trebuchet MS", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(247, 15);
+            this.label2.Location = new System.Drawing.Point(247, 10);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(380, 40);
             this.label2.TabIndex = 0;
@@ -87,7 +87,7 @@
             // 
             this.pictureBox8.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.pictureBox8.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox8.Image")));
-            this.pictureBox8.Location = new System.Drawing.Point(510, 576);
+            this.pictureBox8.Location = new System.Drawing.Point(510, 571);
             this.pictureBox8.Name = "pictureBox8";
             this.pictureBox8.Size = new System.Drawing.Size(50, 31);
             this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -98,7 +98,7 @@
             // 
             this.pictureBox7.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.pictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox7.Image")));
-            this.pictureBox7.Location = new System.Drawing.Point(94, 576);
+            this.pictureBox7.Location = new System.Drawing.Point(94, 571);
             this.pictureBox7.Name = "pictureBox7";
             this.pictureBox7.Size = new System.Drawing.Size(50, 31);
             this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -109,7 +109,7 @@
             // 
             this.pictureBox6.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
-            this.pictureBox6.Location = new System.Drawing.Point(94, 284);
+            this.pictureBox6.Location = new System.Drawing.Point(94, 279);
             this.pictureBox6.Name = "pictureBox6";
             this.pictureBox6.Size = new System.Drawing.Size(50, 31);
             this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -119,7 +119,7 @@
             // button4
             // 
             this.button4.BackColor = System.Drawing.Color.DarkTurquoise;
-            this.button4.Location = new System.Drawing.Point(165, 284);
+            this.button4.Location = new System.Drawing.Point(165, 279);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(103, 31);
             this.button4.TabIndex = 42;
@@ -129,7 +129,7 @@
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.DarkTurquoise;
-            this.button3.Location = new System.Drawing.Point(165, 574);
+            this.button3.Location = new System.Drawing.Point(165, 569);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(103, 31);
             this.button3.TabIndex = 41;
@@ -139,7 +139,7 @@
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.DarkTurquoise;
-            this.button2.Location = new System.Drawing.Point(566, 576);
+            this.button2.Location = new System.Drawing.Point(566, 571);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(103, 31);
             this.button2.TabIndex = 40;
@@ -151,7 +151,7 @@
             this.textBox8.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
             this.textBox8.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox8.ForeColor = System.Drawing.SystemColors.Window;
-            this.textBox8.Location = new System.Drawing.Point(120, 252);
+            this.textBox8.Location = new System.Drawing.Point(120, 247);
             this.textBox8.Name = "textBox8";
             this.textBox8.Size = new System.Drawing.Size(102, 27);
             this.textBox8.TabIndex = 39;
@@ -162,7 +162,7 @@
             this.textBox7.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
             this.textBox7.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox7.ForeColor = System.Drawing.SystemColors.Window;
-            this.textBox7.Location = new System.Drawing.Point(120, 541);
+            this.textBox7.Location = new System.Drawing.Point(120, 536);
             this.textBox7.Name = "textBox7";
             this.textBox7.Size = new System.Drawing.Size(90, 27);
             this.textBox7.TabIndex = 38;
@@ -173,7 +173,7 @@
             this.textBox6.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
             this.textBox6.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox6.ForeColor = System.Drawing.SystemColors.Window;
-            this.textBox6.Location = new System.Drawing.Point(548, 541);
+            this.textBox6.Location = new System.Drawing.Point(548, 536);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(93, 27);
             this.textBox6.TabIndex = 37;
@@ -183,7 +183,7 @@
             // 
             this.textBox5.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
             this.textBox5.ForeColor = System.Drawing.SystemColors.Window;
-            this.textBox5.Location = new System.Drawing.Point(41, 220);
+            this.textBox5.Location = new System.Drawing.Point(41, 215);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(285, 27);
             this.textBox5.TabIndex = 36;
@@ -193,7 +193,7 @@
             // 
             this.textBox4.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
             this.textBox4.ForeColor = System.Drawing.SystemColors.Window;
-            this.textBox4.Location = new System.Drawing.Point(41, 508);
+            this.textBox4.Location = new System.Drawing.Point(41, 503);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(285, 27);
             this.textBox4.TabIndex = 35;
@@ -203,7 +203,7 @@
             // 
             this.textBox3.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
             this.textBox3.ForeColor = System.Drawing.SystemColors.Window;
-            this.textBox3.Location = new System.Drawing.Point(452, 508);
+            this.textBox3.Location = new System.Drawing.Point(452, 503);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(301, 27);
             this.textBox3.TabIndex = 34;
@@ -212,7 +212,7 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.DarkTurquoise;
-            this.button1.Location = new System.Drawing.Point(566, 284);
+            this.button1.Location = new System.Drawing.Point(566, 279);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(103, 31);
             this.button1.TabIndex = 25;
@@ -224,7 +224,7 @@
             this.textBox2.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
             this.textBox2.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox2.ForeColor = System.Drawing.SystemColors.Window;
-            this.textBox2.Location = new System.Drawing.Point(548, 251);
+            this.textBox2.Location = new System.Drawing.Point(548, 246);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(93, 27);
             this.textBox2.TabIndex = 32;
@@ -234,7 +234,7 @@
             // 
             this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
             this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
-            this.textBox1.Location = new System.Drawing.Point(463, 220);
+            this.textBox1.Location = new System.Drawing.Point(463, 215);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(281, 27);
             this.textBox1.TabIndex = 31;
@@ -243,7 +243,7 @@
             // pictureBox4
             // 
             this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-            this.pictureBox4.Location = new System.Drawing.Point(486, 341);
+            this.pictureBox4.Location = new System.Drawing.Point(486, 336);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(209, 152);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -253,7 +253,7 @@
             // pictureBox3
             // 
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(71, 347);
+            this.pictureBox3.Location = new System.Drawing.Point(71, 342);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(209, 152);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -263,7 +263,7 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(486, 56);
+            this.pictureBox2.Location = new System.Drawing.Point(486, 51);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(209, 152);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -273,7 +273,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(71, 56);
+            this.pictureBox1.Location = new System.Drawing.Point(71, 51);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(209, 152);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -284,7 +284,7 @@
             // 
             this.pictureBox5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
-            this.pictureBox5.Location = new System.Drawing.Point(506, 284);
+            this.pictureBox5.Location = new System.Drawing.Point(506, 279);
             this.pictureBox5.Name = "pictureBox5";
             this.pictureBox5.Size = new System.Drawing.Size(50, 31);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
